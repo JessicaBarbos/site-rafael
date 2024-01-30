c967fb14482f31fe40d0b5e9e8d3cf73d4b16ef5
Jéssica Barbosa <jessica@abe3.com.br>
2024-01-29 23:14:35 -0300

    feat: Inicio módulo clientes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -38,6 +39,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::controller(ClientController::class)->group(function () {
+        Route::get('/clientes', 'index');
+        Route::get('/clientes/{id}', 'show');
+        Route::post('/clientes', 'store');
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
